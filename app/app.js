4a1c7f799d1bb5963049d95c5af1389162411f7d
Paul W <p.p.wronski@gmail.com>
2015-12-11 10:15:04 +0100

    computer logo river2 section

--- app/app.js
+++ app/app.js
@@ -4,6 +4,16 @@
 
 $(function() {
     console.log('dziala');
+
+    $(document).ready(function() {
+        $(window).scroll(function(){
+            $('*[class^="prlx"]').each(function(r){
+                var pos = $(this).offset().top;
+                var scrolled = $(window).scrollTop();
+                $('*[class^="prlx"]').css('top', -(scrolled * 0.5) + 'px');
+            });
+        });
+    });
 });
 
 
