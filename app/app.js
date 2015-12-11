861039a991d5064b0b58db19ee03a82b4f9fd127
Paul W <p.p.wronski@gmail.com>
2015-12-11 12:34:08 +0100

    365 part

--- app/app.js
+++ app/app.js
@@ -5,23 +5,6 @@
 $(function() {
     console.log('dziala');
 
-    $(document).ready(function() {
-        $(window).scroll(function(){
-            $('*[class^="prlx"]').each(function(r){
-                var pos = $(this).offset().top;
-                var scrolled = $(window).scrollTop();
-                $('*[class^="prlx"]').css('top', -(scrolled * 0.5) + 'px');
-            });
-        });
-    });
-
-    var bar = $('.value-bar');
-    $(function(){
-        $(bar).each(function(){
-            bar_width = $(this).attr('aria-valuenow');
-            $(this).width(bar_width + '%');
-        });
-    });
 });
 
 
