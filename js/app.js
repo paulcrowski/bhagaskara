f7ce42001252f5ef75e410c581fb9aac929c87c3
Paul W <p.p.wronski@gmail.com>
2015-12-16 10:37:16 +0100

    sticky menu with jquery, class adding navbar-fixed-top, or remove

--- js/app.js
+++ js/app.js
@@ -2,11 +2,22 @@
  * Created by POL on 2015-12-09.
  */
 
-$(function() {
+$(document).ready(function(){
     console.log('dziala');
+    var menu = $(".navbar");
+    var menuOffsetFromTop = menu.position().top;
 
+    $(window).on("scroll", function(event){
+        console.log($(window).scrollTop());
+        console.log(menu.position());
+        if($(window).scrollTop()> menuOffsetFromTop) {
+            menu.addClass("navbar-fixed-top");
+        }
+        else {
+            menu.removeClass("navbar-fixed-top");
+        }
+    });
 });
-
 
 
 
